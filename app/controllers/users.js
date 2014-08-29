7a0a02cbbfa2394cf69ada0e0803587b3c44f979
Melanie Fryman <melanie@frymanet.com>
2014-08-29 10:01:55 -0500

    done

--- app/controllers/users.js
+++ app/controllers/users.js
@@ -1,7 +1,8 @@
 'use strict';
 
 var User = require('../models/user'),
-    Message = require('../models/message');
+    Message = require('../models/message'),
+    moment = require('moment');
 
 exports.new = function(req, res){
   res.render('users/new');
@@ -88,7 +89,7 @@ exports.displayMessages = function(req, res){
   Message.find(res.locals.user._id, function(err, messages){
     var unread = 0;
     messages.forEach(function(m){ unread = (m.isRead) ? unread : unread + 1; });
-    res.render('users/messages', {messages: messages, unread: unread});
+    res.render('users/messages', {messages: messages, unread: unread, moment:moment});
   });
 };
 
@@ -96,6 +97,6 @@ exports.displayMessages = function(req, res){
 exports.readMessage = function(req, res){
   Message.read(req.params.messageId, function(message){
     console.log(message);
-    res.render('users/view-message', {message: message});
+    res.render('users/view-message', {message: message, moment:moment});
   });
 };
